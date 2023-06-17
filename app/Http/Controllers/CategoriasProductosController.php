9310241e251e2701a86532b2a7676d4f08c0afb7
andres174 <zambrasda17@gmail.com>
2023-06-17 17:40:14 -0500

    categoria tienda avance 1

--- app/Http/Controllers/CategoriasProductosController.php
+++ app/Http/Controllers/CategoriasProductosController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\CategoriasProductos;
+use App\Models\CategoriaTienda;
+use App\Models\Tienda;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoriasProductosController extends Controller
 {
@@ -12,7 +15,11 @@ class CategoriasProductosController extends Controller
      */
     public function index()
     {
-        //
+        //Esto es para admin creo XDD
+        $categorias_productos = CategoriasProductos::where('estado', 1)->get();
+
+        return response()->json($categorias_productos, 200);
+
     }
 
     /**
@@ -29,6 +36,36 @@ class CategoriasProductosController extends Controller
     public function store(Request $request)
     {
         //
+        $validData = $request->validate([
+            'descripcion' => 'required|string|max:255',
+            'imagen' => 'null',
+            'id_tienda' => 'required',
+        ]);
+
+        $tienda_guardar = Tienda::find($validData['id_tienda']);
+
+        if (isset($validData['imagen'])) {
+            $img = $request->file('imagen');
+            $validData['imagen'] = time() . '.' . $img->getClientOriginalExtension();
+        } else {
+            $validData['imagen'] = null;
+        }
+
+        $cat_tienda = CategoriaTienda::create([
+            'descripcion' => $validData['descripcion'],
+            'imagen' => $validData['imagen'],
+            'id_tienda' => $validData['id_tienda'],
+            'estado' => 1
+        ]);
+
+        $request->file('imagen')->storeAs("public/images/categorias_tienda/{$tienda_guardar->id}/{$cat_tienda->id}", $validData['imagen']);
+
+        return response()
+        ->json([
+            'message' => 'Categoría de tu tienda registrada',
+            'data' => $cat_tienda   
+        ], 200);
+
     }
 
     /**
@@ -61,5 +98,17 @@ class CategoriasProductosController extends Controller
     public function destroy(CategoriasProductos $categoriasProductos)
     {
         //
+    }
+
+    /* 
+    Con este buscamos las categorias de tiendas por el ID de la tienda
+    */
+    public function categoriasPorTienda($id_tienda){
+        $data = DB::table('categorias_productos')
+        ->where('id_tienda', $id_tienda)
+        ->where('estado', 1)
+        ->get();
+
+        return response($data, 200);
     }
 }
