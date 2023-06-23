6788ff4b82772bf862c8822ea0197d211c1d1647
andres174 <zambrasda17@gmail.com>
2023-06-23 12:03:57 -0500

    Toppings y toppings productos

--- app/Http/Controllers/ToppingsController.php
+++ app/Http/Controllers/ToppingsController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
+use App\Models\Tienda;
 use App\Models\Toppings;
+use App\Models\ToppingsProductos;
 use Illuminate\Http\Request;
 
 class ToppingsController extends Controller
@@ -12,7 +15,8 @@ class ToppingsController extends Controller
      */
     public function index()
     {
-        //
+        //la idea de esto es que el emprendedor pueda 
+        //ver todos los toppings que tiene asi en general
     }
 
     /**
@@ -28,7 +32,67 @@ class ToppingsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //falta probarlo
+        //este siempre va a guardar el nuevo topping 
+        //unido al producto, probablemente esto vaya en un for o algo asi XD
+
+        $validData = $request->validate([
+            'descripcion' => 'required|string|max:255',
+            'precio' => 'required',
+            'id_tienda' => 'required',
+            'imagen' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            //esto puede ser diferente
+            'id_producto' => 'required'
+        ]);
+
+        $p = Producto::find($validData['id_producto']);
+        $t = Tienda::find($validData['id_tienda']);
+
+        if (isset($validData['imagen'])) {
+            $img = $request->file('imagen');
+            $validData['imagen'] = time() . '.' . $img->getClientOriginalExtension();
+        } else {
+            $validData['imagen'] = null;
+        }
+
+        if(isset($p) && isset($t)){
+
+            $topping = Toppings::create([
+                'descripcion' => $validData['descripcion'],
+                'precio' => $validData['precio'],
+                'id_tienda' => $validData['id_tienda'],
+                'imagen' => $validData['imagen'],
+                'estado' => 1
+            ]);
+
+            if($validData['imagen'] != null){
+                $request->file('imagen')->storeAs("public/images/toppings/{$t->id}/{$p->id}/{$topping->id}", $validData['imagen']);
+            }
+
+            $toppings_producto = ToppingsProductos::create([
+                'id_producto' => $validData['id_producto'],
+                'id_toppings' => $topping->id,
+                'estado' => 1
+            ]);
+
+
+            return response()
+            ->json([
+                'message' => 'Categoría de tu tienda registrada',
+                'topping' => $topping,
+                'toppings_producto' => $toppings_producto  
+            ], 200);
+
+
+
+        } else {
+            return response()
+            ->json([
+                'message' => 'El producto o la tienda no existen.',
+                /* 'data' => '', */
+            ], 400);
+        }
+
     }
 
     /**
@@ -52,7 +116,7 @@ class ToppingsController extends Controller
      */
     public function update(Request $request, Toppings $toppings)
     {
-        //
+        //a faltan cosas
     }
 
     /**
