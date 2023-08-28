777ecebeeefc0ad90a8bcee395c28fe321e860ca
gabriel12QK <jogar737@gmail.com>
2023-08-28 15:30:01 -0500

    cambio en migraciones de marca

--- database/migrations/2023_06_04_131543_create_marcas_table.php
+++ database/migrations/2023_06_04_131543_create_marcas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('marcas', function (Blueprint $table) {
             $table->id();
             $table->string('descripcion');
-            $table->string('imagen')->nullable();
+            //$table->string('imagen')->nullable();
             $table->foreignId('id_tienda')->constrained('tiendas');
             $table->boolean('estado'); 
         });
