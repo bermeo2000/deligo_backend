84dd60acbfc29e758f567379b320d78a04fd198b
Luis Macias <luismaciasantonio2000@gmail.com>
2023-09-20 17:03:02 -0500

    cositas

--- routes/api.php
+++ routes/api.php
@@ -111,9 +111,7 @@ Route::resource('categoria-usuario', CategoriasUsuarioController::class);
 
 //Promocion Productos
 Route::resource('promo-productos', PromocionProductoController::class);
-Route::get('get-prod-by-tienda/{id}', [PromocionProductoController::class, 'getProductoByTienda']);
-
-Route::get('get-promo-productos-by-tienda/{id}', [PromocionProductoController::class, 'getPromoProductoTienda']);
+Route::get('get-promo-productos-by-tienda/{id}', [PromocionProductoController::class, 'getPromocionProductoByTienda']);
 
 //Toppings
 Route::resource('toppings', ToppingsController::class);
