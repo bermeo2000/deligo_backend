89c3254021999f4face5f14091895258363553e5
andres174 <zambrasda17@gmail.com>
2023-11-15 09:09:19 -0500

    seeder directo a prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
             CategoriasTiendasSeeder::class,
             CodigoPaisSeeder::class,
             UsuarioSeeder::class,
-            TiendasSeeder::class,
-            MarcasSeeder::class,
+            //TiendasSeeder::class,
+            //MarcasSeeder::class,
             //CategoriaProductoSeeder::class,
             //ProductosSeeder::class
         ]);
